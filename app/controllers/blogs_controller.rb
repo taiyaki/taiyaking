5066d98f9b4d5c1089501337814c95ebfa504ce0
Koichiro Ohba <koichiro@meadowy.org>
2009-03-03 19:51:22 +0900

    Added error handling.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,13 +10,18 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
     @blog.user = logined_user
+    @title = "Blogエントリ作成 - たいやきる？"
   end
 
   def create
-    blog = Blog.new(params[:blog])
-    blog.user = logined_user
-    blog.save
-    redirect_to :action => 'show', :id => blog.id
+    @blog = Blog.new(params[:blog])
+    @blog.user = logined_user
+    if @blog.save
+      flash[:notice] = '新しいエントリを追加しました'
+      redirect_to(@blog)
+    else
+      render :action => "new"
+    end
   end
 
   def show
@@ -30,15 +35,16 @@ class BlogsController < ApplicationController
   def edit
     @blog = Blog.find(params[:id])
     @blog.user = logined_user
-  end
+    @title = "Blogエントリ編集 - たいやきる？"
+ end
 
   def update
-    blog = Blog.find(params[:id])
-    blog.attributes = params[:blog]
-    blog.user = logined_user
-    if blog.save
-      flash[:notice] = '更新しました'
-      redirect_to :action => 'show', :id => blog.id
+    @blog = Blog.find(params[:id])
+    @blog.attributes = params[:blog]
+    @blog.user = logined_user
+    if @blog.save
+      flash[:notice] = 'エントリを更新しました'
+      redirect_to(@blog)
     else
       render :action => 'edit'
     end
