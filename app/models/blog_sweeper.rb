76fda8e12b6348135a4b9422518ca17924db2a0b
Koichiro Ohba <koichiro@meadowy.org>
2009-03-06 01:16:17 +0900

    Added cache expires.

--- app/models/blog_sweeper.rb
+++ app/models/blog_sweeper.rb
@@ -4,5 +4,8 @@ class BlogSweeper < ActionController::Caching::Sweeper
   def after_save(record)
     expire_page(:controller => "front", :action => "index")
     expire_page(:controller => "feed", :action => "index")
+    expire_page(:controller => "feed", :action => "index", :format => "rdf")
+    expire_page(:controller => "feed", :action => "index", :format => "rss")
+    expire_page(:controller => "feed", :action => "index", :format => "atom")
   end
 end
