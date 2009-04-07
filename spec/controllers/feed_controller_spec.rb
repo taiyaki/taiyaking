1c973e4b3326707ee7a4147bd99b8ed5864479fb
Kouhei Sutou <kou@cozmixng.org>
2009-04-07 20:27:44 +0900

    * use UTC in system and convert time to local time on display.

--- spec/controllers/feed_controller_spec.rb
+++ spec/controllers/feed_controller_spec.rb
@@ -15,6 +15,16 @@ describe FeedController do
       response.should be_success
     end
 
+    it "should be correct timezone" do
+      get 'index'
+      items = RSS::Parser.parse(response.body).items
+      expected = [
+                  Time.parse("2009-04-07T20:10:30+00:00").utc,
+                  Time.parse("2009-04-01T00:11:22+00:00").utc,
+                 ]
+      items.collect(&:dc_date).should == expected
+    end
+
     it "should return NOT FOUND when no post exist" do
       Blog.destroy_all
       get 'index'
