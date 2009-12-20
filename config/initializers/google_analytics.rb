130a0510592f386add5bc8dda5ac4d1cd43cd16a
Kouhei Sutou <kou@cozmixng.org>
2009-12-20 19:27:12 +0900

    accept the last newline.

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,4 +1,4 @@
 google_analytics_code = Rails.root + "config" + "google-analytics-code.txt"
 if google_analytics_code.exist?
-  GOOGLE_ANALYTICS_CODE = File.read(google_analytics_code)
+  GOOGLE_ANALYTICS_CODE = File.read(google_analytics_code).strip
 end
