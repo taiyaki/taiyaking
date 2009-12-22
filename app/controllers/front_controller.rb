7923959b827fc872ed63703fdf839cb5eb80074d
Kouhei Sutou <kou@cozmixng.org>
2009-12-22 21:48:21 +0900

    remove sub title from top page.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -3,7 +3,6 @@ class FrontController < ApplicationController
   caches_page :index
 
   def index
-    @title = "最近の活動状況"
     blogs = Blog.find(:all, :order => "updated_at DESC", :limit => 5)
     @recent = recent_blog(blogs)
   end
