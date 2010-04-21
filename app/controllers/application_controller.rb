749c5decd1e1535cbacebb776ef9440d8e636d00
Kouhei Sutou <kou@clear-code.com>
2010-04-22 00:36:10 +0900

    remiove needless code.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,6 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '69358521fb1bd90470318198f05f3855'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  filter_parameter_logging :password
 
   private
   def block_until_authorized
