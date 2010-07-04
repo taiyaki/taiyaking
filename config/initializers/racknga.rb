033d4c1f1e7647f916a554e829b0f0436e368c5a
Kouhei Sutou <kou@clear-code.com>
2010-07-04 21:32:28 +0900

    fix a typo.

--- config/initializers/racknga.rb
+++ config/initializers/racknga.rb
@@ -3,7 +3,7 @@ if smtp_yml.exist?
   smtp_configurations = YAML.load(smtp_yml.read)
   smtp_configuration = smtp_configurations[Rails.env]
   if smtp_configuration
-    notifiers = [Racknga::ExceptionMailNotifier.new(searcher_options[:smtp])]
+    notifiers = [Racknga::ExceptionMailNotifier.new(smtp_configuration)]
     config = Rails.application.config
     config.middleware.insert_after(ActionDispatch::ShowExceptions,
                                    Racknga::Middleware::ExceptionNotifier,
