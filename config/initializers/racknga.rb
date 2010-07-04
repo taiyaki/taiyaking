b8b188b9796ab5ec718d14defbf764dd33d121f5
Kouhei Sutou <kou@clear-code.com>
2010-07-04 21:54:44 +0900

    add missing 'nkf' for racknga 0.9.0.

--- config/initializers/racknga.rb
+++ config/initializers/racknga.rb
@@ -3,6 +3,7 @@ if smtp_yml.exist?
   smtp_configurations = YAML.load(smtp_yml.read)
   smtp_configuration = smtp_configurations[Rails.env]
   if smtp_configuration
+    require 'nkf' # for racknga 0.9.0
     notifiers = [Racknga::ExceptionMailNotifier.new(smtp_configuration)]
     config = Rails.application.config
     config.middleware.insert_after(ActionDispatch::ShowExceptions,
